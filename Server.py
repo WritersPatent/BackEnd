6bb900c3965aad895cd8707654c0a65c2e95aafc
midoriSJ <106817383+midoriSJ@users.noreply.github.com>
2024-11-08 16:07:16 +0900

    Update Server.py

--- Server.py
+++ Server.py
@@ -14,17 +14,3 @@ socketio = SocketIO(app, cors_allowed_origins = "*")
 
 if __name__ == "__main__":
     socketio.run(app, debug=True, host="0.0.0.0", port=5001)
-=======
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-import mysql.connector
-
-
-# Flask 애플리케이션 초기화
-app = Flask(__name__)
-CORS(app)
-
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5001)
->>>>>>> 1606b7a6a5643ae774d1bffc12dbc21363342794
